aa15c9dfd46ffc4da21f126679023399db5e36aa
kangkyu <kangkyu1111@gmail.com>
2015-09-25 20:10:17 -0700

    Add user_id to Session Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921044527) do
+ActiveRecord::Schema.define(version: 20150926030932) do
 
   create_table "haikus", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150921044527) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "oauth_token"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
