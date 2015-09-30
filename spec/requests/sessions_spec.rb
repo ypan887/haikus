315649de006820cde0ccf0ea3407f25cb39559bf
kangkyu <kangkyu1111@gmail.com>
2015-09-30 14:52:47 -0700

    bug fix for sessions controller create method unsuccessful
    add an action for '/auth/failure' redirected route as github login failed
    test "invalid_credential" for auth_hash in unsuccessful login

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'sessions', type: :request do
+describe 'sessions create successful', type: :request do
 
   before :each do
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
@@ -24,6 +24,34 @@ describe 'sessions', type: :request do
 
 end
 
+describe 'sessions create unsuccessful', type: :request do
+
+  before :each do
+    OmniAuth.config.mock_auth[:github] = :invalid_credentials
+  end
+
+  it 'does not add a session' do
+    expect {
+      get '/auth/github/callback'
+    }.to change{ Session.count }.by(0)
+
+    expect(response.status).to eq(302)
+    # You will be redirected back to /auth/failure?message=invalid_credentials.
+  end
+
+  it 'does not add a user' do
+    expect {
+      get '/auth/github/callback'
+    }.to change{ User.count }.by(0)
+  end
+
+  it 'render status 400' do
+    get '/auth/failure'
+    expect(response.status).to eq(400)
+  end
+
+end
+
 describe 'sessions destroy', type: :request do
   session = Session.create(user_id: 1, oauth_token: "MyString")
 
@@ -31,6 +59,7 @@ describe 'sessions destroy', type: :request do
     expect {
       delete "/sessions/#{session.id}"
     }.to change{ Session.count }.by(-1)
+
     expect(response.status).to eq(204)
   end
 end
