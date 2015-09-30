9ea5a773b355d87872318ae7ca257994631694a9
Richard Cheng <rlcheng.7@gmail.com>
2015-09-29 23:05:53 -0700

    Add Tag model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926030932) do
+ActiveRecord::Schema.define(version: 20150929205233) do
 
   create_table "haikus", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20150926030932) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.integer  "haiku_id"
+    t.string   "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
